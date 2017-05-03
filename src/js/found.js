275ea1c9be3cb7486e06e8f5d726c7f659426bdf
wad1123@qq.com <wad1123@qq.com>
2017-05-03 18:40:06 +0800

    use angular structrue

--- src/js/found.js
+++ src/js/found.js
@@ -1,12 +1,3 @@
 function Found () {
     var app = window.myapp,scroll;
-    $( '#container' ).html( app.eleArr[2][0] );
-    scroll = new fz.Scroll('#tab3', {
-        role: 'tab'
-    });
-    itcast.iScroll({
-        swipeDom:$( '.fd_iconTitle' )[0],
-        swipeType:'x',
-        swipeDistance:150
-    });
 }
--- src/js/index.js
+++ src/js/index.js
@@ -2,62 +2,47 @@
     var app = window.myapp;
     app.controller( 'controller',['$scope',function ($scope) {
 
-    }] );
+    }] )
+    .controller( 'rklist',[ '$scope',function ($scope) {
+        new fz.Scroll('#tab1', {
+            role: 'tab',
+            autoplay: false,
+            interval: 5000
+        });
+    } ] )
+    .controller( 'tolisten',[ '$scope',function ( $scope ) {
+        new fz.Scroll('#tab2', {
+            role: 'tab',
+            autoplay: false,
+            interval: 5000
+        });
+    } ] )
+    .controller( 'found',[ '$scope',function ( $scope ) {
+        scroll = new fz.Scroll('#tab3', {
+            role: 'tab'
+        });
+        itcast.iScroll({
+            swipeDom:$( '.fd_iconTitle' )[0],
+            swipeType:'x',
+            swipeDistance:150
+        });
+    } ] );
 
-    app.directive( 'rkListq',[function () {
-        return {
-            templateUrl:'src/template/rkList.html',
-            restrict:'ECA',
-            scope:{},
-            replace:true,
-            link:function ( scope,ele,attr ) {
-                app.eleArr.push( ele );
-                $( 'footer li:nth-of-type(1)' ).click( rkList ).trigger( 'click' );
-                new fz.Scroll('#tab1', {
-                    role: 'tab',
-                    autoplay: false,
-                    interval: 5000
-                });
-            }
-        }
-    }] );
-    app.directive( 'toListen',[function () {
-        console.log( 'toListen' );
-        return {
-            templateUrl:'src/template/Listen.html',
-            restrict:'ECA',
-            scope:{},
-            replace:true,
-            link:function ( scope,ele,attr ) {
-                app.eleArr.push( ele );
-                $( 'footer li:nth-of-type(2)' ).click( Listen );
-            }
-        }
-    }] );
-    app.directive( 'toFound',[function () {
-        return {
-            templateUrl:'src/template/found.html',
-            restrict:'ECA',
-            scope:{},
-            replace:true,
-            link:function ( scope,ele,attr ) {
-                app.eleArr.push( ele );
-                $( 'footer li:nth-of-type(3)' ).click( Found );
-            }
-        }
-    }] );
-    app.directive( 'mineSite',[function () {
-        return {
-            templateUrl:'src/template/mine.html',
-            restrict:'ECA',
-            scope:{},
-            replace:true,
-            link:function ( scope,ele,attr ) {
-                app.eleArr.push( ele );
-                $( 'footer li:nth-of-type(4)' ).click( mine );
-            }
-        }
-    }] );
-
+    app.config( function ( $routeProvider ) {
+        $routeProvider.when( '/rklist',{
+            templateUrl:'./src/template/rkList.html',
+            controller:'rklist'
+        } ).when( '/tolisten',{
+            templateUrl:'./src/template/Listen.html',
+            controller:'tolisten'
+        } ).when( '/tofound',{
+            templateUrl:'./src/template/found.html',
+            controller:'found'
+        } ).when( '/mineSite',{
+            templateUrl:'./src/template/mine.html'
+        } ).otherwise({
+            redirectTo:'/rklist'
+        });
+    } );
 
 }();
